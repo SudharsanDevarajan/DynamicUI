64edb43b976e2bf25c44c1fd552bdc1312776007
Sudharsan D <devarajsudharsan@gmail.com>
2023-10-26 11:32:32 +0530

    Initial Commit

---
+++ DynamicUI/DynamicUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicUIApp.swift
+//  DynamicUI
+//
+//  Created by htcuser on 26/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DynamicUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
