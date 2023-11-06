5d9242573c814bfcdecea8cd1abbf82e28da4961
Sudharsan D <devarajsudharsan@gmail.com>
2023-11-06 17:51:42 +0530

    Bug fixes

---
+++ DynamicUI/Components/DropdownView.swift
@@ -0,0 +1,18 @@
+//
+//  DropdownView.swift
+//  DynamicUI
+//
+//  Created by htcuser on 06/11/23.
+//
+
+import SwiftUI
+
+struct DropdownView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DropdownView()
+}
